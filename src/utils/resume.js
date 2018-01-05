9397de209ebbfe510c4a42584868fb6b01deef29
tangxuening <584911730@qq.com>
2018-01-05 09:03:42 +0800

    Update resume.js

--- src/utils/resume.js
+++ src/utils/resume.js
@@ -1,20 +1,20 @@
 export default {
     'basics': {
-        'position': 'web前端工程师',
-        'name': '王小勇',
+        'position': 'java开发工程师',
+        'name': '汤薛宁',
         'gender': '男',
         'age': '25',
-        'worklife': '1.5',
+        'worklife': '3',
         'label': 'Different but not less',
-        'picture': 'http://oaxfgzulv.bkt.clouddn.com/avatar.jpg',
-        'email': '838432498@qq.com',
-        'phone': '18333116993',
+        'picture': 'http://oaxfgzulv.bkt.clouddn.com/tangdatang_avatar.jpg',
+        'email': '584911730@qq.com',
+        'phone': '18233136948',
         'website': 'https://github.com/zincode',
         'summary': [
-            '个人主攻 web 前端方向，有一定nodejs和php基础。',
-            '喜欢关注 web 前端相关新技术，对于新技术有很强的学习兴趣。',
-            '喜欢设计界面优美的程序',
-            '热爱前端这份工作，也热爱一切美好的事物，对于我来说，是一种责任、是一种快乐。'
+            '个人主攻 java后台开发方向，有一定python和linux基础。',
+            '喜欢关注 java开发 相关新技术，对于新技术有很强的学习兴趣。',
+            '喜欢设计代码简洁的程序',
+            '热爱开发这份工作，也热爱一切美好的事物，对于我来说，是一种责任、是一种快乐。'
         ],
         'location': '北京昌平区',
         'profiles': [
@@ -22,88 +22,108 @@ export default {
                 'network': 'github',
                 'username': '',
                 'url': 'https://github.com/ZinCode'
-            },
-            {
-                'network': 'zhihu',
-                'username': 'JSMotion',
-                'url': 'https://www.zhihu.com/people/hzimmer/activities'
             }
+            
         ]
     },
     'work': [
-        {
-            'company': '北京鲜柚科技有限公司',
-            'position': 'web前端工程师',
+	{
+            'company': '北京和鸿盈科科技有限公司',
+            'position': 'java开发工程师',
             'website': '',
-            'startDate': '2016-10',
-            'endDate': '2017-08',
-            'summary': '北京鲜柚科技有限公司',
+            'startDate': '2017-06',
+            'endDate': '至今',
+            'summary': '北京和鸿盈科科技有限公司',
             'highlights': [
-                '庖丁项目开发组',
-                '使用vue框架编写债有主官网',
-                '重构债有主微信网页版代码',
-                '开发相应债有主微信小程序',
-                '负责AutoDoc网站功能模块及后台管理系统的设计',
-                '开发waterfall前台功能页面',
-                '负责债有主网站功能模块及后台管理系统的设计',
-                '负责配合后台开发人员，完成页面的交互及接口对接'
+                '联通科研项目备案及评估系统维护及二次开发', 
+                '联通分公司结算评奖系统',
+                '完成后台接口设计与编写'
+            ]
+        },
+        {
+            'company': '北京水木元生科技有限公司',
+            'position': 'java开发工程师',
+            'website': '',
+            'startDate': '2016-03',
+            'endDate': '2017-05',
+            'summary': '北京水木元生科技有限公司',
+            'highlights': [
+                '健康一体机系统', 
+                '软硬件接口交互开发',
+                '服务器部署及调试',
+                '开发健康一体机自更新软件',
+                '负责配合前端开发人员，完成页面的交互及接口对接'
+            ]
+        },
+	
+            'company': '河北圣诺联合科技有限公司',
+            'position': 'java开发工程师',
+            'website': '',
+            'startDate': '2015-06',
+            'endDate': '2016-01',
+            'summary': '河北圣诺联合科技有限公司',
+            'highlights': [
+                '电子招投标开发组', 
+                '使用ssm框架编写招投标业务逻辑',
+                '负责招标网站功能模块及后台管理系统的设计',
+                '负责配合测试人员，进行页面和接口测试'
             ]
         },
     ],
     'project': [
         {
-            'organization': '债有主官网、债有主后台管理、AutoDoc后台管理、waterfall后台管理',
-            'position': 'web前端工程师',
+            'organization': '招投标网站、招投标后台业务管理',
+            'position': java开发工程师',
             'website': '',
-            'startDate': '2016.10',
-            'endDate': '2017.03',
-            'summary': 'Vue2.0 + Vue-router + Vuex + elementUI + webpack + ES6 + postcss',
+            'startDate': '2015.09',
+            'endDate': '2016.01',
+            'summary': 'Eclipse + tomcat + jsp+ easyUI + Oracle',
             'highlights': [
-                '基于 vue + elementUI 前端框架开发，采用 webpack 作为自动化构建工具',
-                '基于 nodejs 开发环境，使用 webpack 自动化构建工具，实时编译代码，热更新',
-                '采用 vuex 管理前端数据流，其中债有主官网实现微信登陆以及支付功能',
-                '使用vue2.0重构债有主后台管理，使操作更加方便，优化代码质量'
+                '基于 easyUI 前端框架开发，使界面更简洁，提升用户体验',
+                '采用MVC分层架构，使业务逻辑更简明，提高复用性',
+                '使用PowerDesigner进行数据建模，优化数据库逻辑关系',
+                '熟悉linux，能进行项目服务部署'
             ]
         },
         {
-            'organization': '债有主微信网页',
-            'position': 'web前端工程师',
-            'website': '公众号债有主',
-            'startDate': '2017.03',
+            'organization': '健康一体机系统',
+            'position': 'java开发工程师',
+            'website': '',
+            'startDate': '2016.04',
             'endDate': '2017.04',
-            'summary': 'Vue2.3 + Vuex + Vue-router + MintUI + Webpack + ES7 + jQueryMobileUI',
+            'summary': 'IDEA+SSM+MySQL+RPC、Redis、angularJs、Python+mongoDB',
             'highlights': [
-                '使用vue + mintUI + jqueryMobileUI前端框架开发，使用ES7最新语法，async/await处理异步请求',
-                '实现用户的浏览、订阅、分享、标记、事件提醒、查看消息记录等常用功能，',
-                '通过可视化公式来查看金融文档符合工具',
+                '基于SSM框架开发，提升运行效率，熟练使用分布式RPC框架',
+                '网站开发使用angularJS，双向数据绑定使界面更友好',
+                '使用Python爬取大量中医相关数据，方便与一体机测量数据做分析',
+ 		'软硬件接口交互开发',
+		'熟练Redis应用，熟悉缓存击穿和缓存崩溃',
             ]
         },
         {
-            'organization': '债有主微信小程序',
-            'position': 'web前端工程师',
-            'website': '对应债有主微信小程序',
-            'startDate': '2017.04',
-            'endDate': '2017.05',
-            'summary': '微信小程序',
+            'organization': '联通科研项目备案及评估系统',
+            'position': 'java开发工程师',
+            'website': '',
+            'startDate': '2017.10',
+            'endDate': '至今',
+            'summary': 'IDEA+Tomcat+Hibernate+Oracle',
             'highlights': [
-                '基于微信小程序自有框架开发，使用promise全局处理微信异步请求接口',
-                '实现功能与债有主微信网页版相同，关联相应公众号，优化用户体验',
-                '抽离公共组件，增强代码复用性，关联原有PC用户，打通整个用户数据'
+                '基于已有工作流进行二次开发，根据客户需求对系统进行扩展和修改',
+                '代码测试及服务器部署',
+                '抽离公共组件，增强代码复用性'
             ]
         }
     ],
     'education': [
         {
             'institution': '河北科技大学',
-            'area': '自动化',
+            'area': '计算机技术',
             'studyType': '本科',
             'startDate': '2011-09',
             'endDate': '2015-06',
             'gpa': '4.0',
             'courses': [
-                'C语言',
-                '模电与数电',
-                '微机原理'
+                
             ]
         }
     ],
@@ -115,19 +135,7 @@ export default {
             'summary': ''
         }
     ],
-    'publications': [
-        {
-            'name': '蝶恋果微信小程序',
-            'publisher': 'JSM',
-            'releaseDate': '2017-10',
-            'website': 'https://github.com/ZinCode/dielianguo',
-            'summary': '微信小程序-商城，提供商品浏览，分类，下单，支付(未完善)等一系列电商的基础功能',
-            'highlights': [
-                'asdfasdf'
-            ]
-
-        }
-    ],
+   
     'skills': [
         {
             'name': '前端',
@@ -136,22 +144,21 @@ export default {
                 'HTML',
                 'CSS',
                 'Javascript',
-                'Sass',
-                'Postcss',
+                'angularJs',
                 'Webpack',
-                'Vue',
-                'React',
-                '微信小程序'
             ]
         },
         {
             'name': '后端',
             'level': 'master',
             'keywords': [
-                'Nodejs',
-                'express',
-                'PHP',
-                'thinkphp5.0'
+                'java',
+                'SSM',
+                'RPC',
+		'MySQL',
+		'ORACLE',
+		'PYTHON',
+                'REDIS'
             ]
         }
     ],
@@ -166,16 +173,12 @@ export default {
             'name': '社交',
             'keywords': [
                 '知乎',
-                '掘金',
                 '微信',
-                '虾米',
-                '网易云'
             ]
         },{
             'name': '爱好',
             'keywords': [
-                '骑行',
-                '果粉',
+                '骑行脑残粉',
                 '会修电脑😄'
             ]
         }
